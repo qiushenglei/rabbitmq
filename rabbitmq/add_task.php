116ab0c78c824366421d97fede8617748c8c66a5
qsl <qsl.@qq.com>
2018-09-25 21:16:03 +0800

    rabbit

---
+++ rabbitmq/add_task.php
@@ -0,0 +1,27 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+//$msg = new AMQPMessage('Hello World!');
+//$channel->basic_publish($msg, '', 'hello');
+
+//echo " [x] Sent 'Hello World!'\n";
+
+$data = implode(' ', array_slice($argv, 1));
+if (empty($data)) {
+    $data = "Hello World!";
+}
+$msg = new AMQPMessage($data);
+
+$channel->basic_publish($msg, '', 'hello');
+
+echo ' [x] Sent ', $data, "\n";
+
+$channel->close();
+$connection->close();
---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,33 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yidas/yii2-bower-asset' => 
+  array (
+    'name' => 'yidas/yii2-bower-asset',
+    'version' => '2.0.13.0',
+    'alias' => 
+    array (
+      '@yidas/yii2BowerAsset' => $vendorDir . '/yidas/yii2-bower-asset',
+    ),
+  ),
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '2.0.8.0',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap/src',
+    ),
+  ),
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => '2.1.1.0',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer/src',
+    ),
+  ),
+);
